2ea0178d8d877e3339a52c7e7d92d768c228a0bc
LEANDRO SILVA <leandro.silva1001@etec.sp.gov.br>
2021-08-09 18:26:13 -0300

    Adicionar arquivos de projeto.

---
+++ LojaDeJogos/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaDeJogos.Models
+{
+    public class Cliente : Controller
+    {
+        // GET: Cliente
+        public string ClienteNome { get; set; }
+        public string ClienteCPF { get; set; }
+        public DateTime FuncDtNasc { get; set; }
+        public string ClienteEmail { get; set; }
+
+        public string ClienteCel { get; set; }
+        public string ClienteEnd { get; set; }
+    }
+}
