4b1094805dcbd1267c2120e6802d5969c3bce5a3
LEANDRO SILVA <leandro.silva1001@etec.sp.gov.br>
2021-08-09 18:26:13 -0300

    Adicionar arquivos de projeto.

---
+++ LojaDeJogos/Models/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaDeJogos.Models
+{
+    public class Funcionario : Controller
+    {
+        // GET: Funcionario
+        public ushort FuncCod { get; set; }
+
+        public string FuncNome { get; set; }
+
+        public string FuncCPF { get; set; }
+
+        public string FuncRg { get; set; }
+
+        public DateTime FuncDtNasc { get; set; }
+
+        public string FuncEnd { get; set; }
+
+        public string FuncCel { get; set; }
+
+        public string FuncEmail { get; set; }
+
+        public string FuncCargo { get; set; }
+
+    }
+}
---
+++ LojaDeJogos/Models/Jogo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaDeJogos.Models
+{
+    public class Jogo : Controller
+    {
+        // GET: Jogo
+        public ushort JogoCod { get; set; }
+        public string JogoNome { get; set; }
+        public int JogoVersao { get; set; }
+        public string JogoDesenv { get; set; }
+        public string JogoGen { get; set; }
+        public int JogoFaixaEt { get; set; }
+        public string JogoPlat { get; set; }
+        public int JogoAnoLanc { get; set; }
+
+    }
+}
