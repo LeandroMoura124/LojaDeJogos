078bf2dacf8d3d59da9957629a780b2c6ab10478
LeandroMoura124 <leofcb334@gmail.com>
2021-08-11 12:07:42 -0300

    Arquivo do projeto.

--- LojaDeJogos/Models/Cliente.cs
+++ LojaDeJogos/Models/Cliente.cs
@@ -11,8 +11,10 @@ namespace LojaDeJogos.Models
     {
         // GET: Cliente
 
-        [Required(ErrorMessage = "O nome é obrigatório")]
+        [Required(ErrorMessage = "O nome do cliente é obrigatório!")]
         public string ClienteNome { get; set; }
+
+        [StringLength(12, MinimumLength = 9, ErrorMessage = "CPF incompleto ou inválido." )]
         public string ClienteCPF { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyy}", ApplyFormatInEditMode = true)]
@@ -31,10 +33,12 @@ namespace LojaDeJogos.Models
         }
         private DateTime? funcDtNasc = null;
 
-
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Email inválido.")]
         public string ClienteEmail { get; set; }
 
         public string ClienteCel { get; set; }
+
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Informe seu endereço completo.")]
         public string ClienteEnd { get; set; }
     }
 }
